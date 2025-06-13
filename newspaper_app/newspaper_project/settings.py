656f7745f98a7e3d122f5e7732abdb2cd8648565
OlgerQuispe <goldsag43@gmail.com>
2025-06-13 18:52:54 -0500

    update settings: allowed hosts

--- newspaper_app/newspaper_project/settings.py
+++ newspaper_app/newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h)@@0v$vn&@h$7!d4^vxu$-cu7sed^tqk#h!58y5mqwy38#$(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1'] 
 
 
 # Application definition
